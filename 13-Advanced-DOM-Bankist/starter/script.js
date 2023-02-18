c469c83e0dfc2aafc60790758c2000525ca7077b
eliasmartinzs <rhyanlucaseliasmartins@gmail.com>
2023-02-18 15:03:26 -0300

    siblings

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -4,11 +4,11 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav');
 const navLinks = document.querySelector('.nav__links');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 const tabs = document.querySelectorAll('.operations__tab');
-
 
 // Modal window
 const openModal = function () {
@@ -25,25 +25,25 @@ btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
 
-navLinks.addEventListener('click', e => {
-  e.preventDefault();
+navLinks.addEventListener('click', event => {
+  event.preventDefault();
 
-  if (e.target.classList.contains('nav__link')) {
-    const id = e.target.getAttribute('href');
+  if (event.target.classList.contains('nav__link')) {
+    const id = event.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({
       behavior: 'smooth',
     });
   }
 });
 
-tabsContainer.addEventListener('click', function (e) {
-  const clicked = e.target.closest('.operations__tab');
+tabsContainer.addEventListener('click', function (event) {
+  const clicked = event.target.closest('.operations__tab');
 
   if (!clicked) return;
 
@@ -57,3 +57,16 @@ tabsContainer.addEventListener('click', function (e) {
     .classList.add('operations__content--active');
 });
 
+const handler = function (event) {
+  if (event.target.classList.contains('nav__link')) {
+    const link = event.target;
+    const siblins = link.closest('.nav').querySelectorAll('.nav__link');
+
+    siblins.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener('mouseover', handler.bind(0.7));
+nav.addEventListener('mouseout', handler.bind(1));
